8dd9ff92de33d47cbf2bda306077295586d2a747
Joshua Ryandafres Pangaribuan <Joshua.Pangaribuan@virgoku.com>
2022-11-23 14:57:28 +0700

    Added Struct Implementation
    Added implementation to convert struct to map by json tag as a key

--- utils/structOperation.go
+++ utils/structOperation.go
@@ -1,7 +1,40 @@
 package utils
 
-import "github.com/fatih/structs"
+import (
+	"errors"
+	"github.com/fatih/structs"
+	"reflect"
+)
 
-func StructToMapInterface(obj interface{}) map[string]interface{} {
-	return structs.Map(obj)
+func GetJSONTag(obj interface{}) ([]string, error) {
+	// Check if any obj is type struct or not
+	rt := reflect.TypeOf(obj)
+	if rt.Kind() != reflect.Struct {
+		return make([]string, 0), errors.New("parameter is not struct")
+	}
+	jsonTags := make([]string, 0)
+
+	for index := 0; index < rt.NumField(); index++ {
+		field := rt.Field(index)
+		jsonTags = append(jsonTags, field.Tag.Get("json"))
+	}
+	return jsonTags, nil
+}
+
+func ParseStructToJsonMap(obj interface{}) (map[string]string, error) {
+	jsonTags, err := GetJSONTag(obj)
+	if err != nil {
+		return make(map[string]string), errors.New("unable to get struct json tag")
+	}
+	newStructMap := make(map[string]string)
+	oldStructMap := structs.Map(obj)
+
+	jsonTagIndex := 0
+	for _, val := range oldStructMap {
+		strVal, ok := val.(string)
+		if ok {
+			newStructMap[jsonTags[jsonTagIndex]] = strVal
+		}
+	}
+	return newStructMap, nil
 }
